3a9fe7ea3b378760fb5bc69b8b9130e94eb4553b
blackhost <kambaipraise@gmail.com>
2025-10-28 00:40:50 -0400

    feat(analytics): add Vercel Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
